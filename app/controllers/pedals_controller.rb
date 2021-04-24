e29b51920abd37f56e31f5c2317a80c144fb992b
Carlj0666 <carlj1881@yahoo.com>
2021-04-23 20:25:13 -0700

    updated seed file

--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -1,2 +1,7 @@
 class PedalsController < ApplicationController
+  def index
+    pedals = Pedal.all
+    render json: pedals.to_json(except: [:created_at, :updated_at])
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@
 empress = Brand.create(name: 'Empress')
 neunaber = Brand.create(name: 'Neunaber')
 walrus = Brand.create(name: 'Walrus')
+
+# ADD PEDALS
+
+zoia = Pedal.create({name: 'Zoia', price: 499.00, effect: "Multi"})
+
+Pedal.create([
+    {name: 'Zoia', price: 499.99,  effect: 'Multi', brand: empress},
+    {name: 'Immerse', price: 199.99, effect: 'Reverb', brand: neunaber},
+    {name: 'Slarp', price: 349.99,  effect: 'Delay', brand: walrus},
+])
