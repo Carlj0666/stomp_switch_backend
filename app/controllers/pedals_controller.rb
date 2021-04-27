ee7cddd946c4aaffd3b73b0be8c0c341c4e1ed64
Carlj0666 <carlj1881@yahoo.com>
2021-04-26 17:17:45 -0700

    added create controller action to pedals constroller. Woking on strong params

--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -8,5 +8,17 @@ class PedalsController < ApplicationController
     pedal = Pedal.find(params[:id])
     render json: PedalSerializer.new(pedal)
   end
+
+  def create
+    pedal = Pedal.new(pedalParams)
+    if pedal.save
+      render json: PedalSerializer.new(pedal)
+    else 
+      render json: {error: "ERROR line 17 in pedals controller"}
+  end
+
+  private
+  def pedalParams
+    params.require(:pedal).permit(:name, :price, :effect, :image_link, :brand_name)
   
 end
