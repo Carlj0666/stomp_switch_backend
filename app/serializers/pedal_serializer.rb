af016a2182afefafd7a442faeb4b90d6068eea65
Carlj0666 <carlj1881@yahoo.com>
2021-05-05 19:12:46 -0700

    no updates, just in case

--- app/serializers/pedal_serializer.rb
+++ app/serializers/pedal_serializer.rb
@@ -5,15 +5,4 @@ class PedalSerializer
   belongs_to :brand
   attributes :name, :effect, :price, :brand_name, :image_link, :brand_id
 
-  # This adds "effects pedal" to the end of the name, or any other thing to customize
-  # attribute :name do |object|
-  #   "#{object.name} effects pedal"
-  # end
-
-  #model method, for more customization (must include in the serializer attributes list)
-  # def capitalized_name
-  #   self.name.upcase
-  # end
-
-
 end
